ff8b4f8eb122b9a6965198442c4cd607746779b7
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2022-12-14 07:09:14 +0530

    Create main.cpp

---
+++ Codes/Reverse_Number/using_function/Using for Loop/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Reverse()
+    {
+        int Number;
+        int Reverse_Number=0;
+        int Reminder;
+        
+        cout<<"Enter a Number: ";
+        cin>>Number;
+        for(;Number>0;Number/=10)
+        {
+            Reminder=Number%10;
+            Reverse_Number=(Reverse_Number*10)+Reminder;
+        }
+        cout<<"Reverse Number is: "<<Reverse_Number;
+    }
+    
+int main()
+{
+    Reverse();
+}
