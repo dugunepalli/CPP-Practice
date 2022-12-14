c452a3fc7cb8b96e4fdafea8f83f6648016d668d
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2022-12-14 06:42:52 +0530

    Create main.cpp

---
+++ Codes/Reverse_Number/Using_main/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int Number;
+    int Reverse_Number=0;
+    int Reminder;
+    
+    cout<<"Enter a Number: ";
+    cin>>Number;
+    
+    while(Number>0)
+    {
+        Reminder=Number%10;
+        Reverse_Number=(Reverse_Number*10)+Reminder;
+        Number=Number/10;
+    }
+    cout<<"Reverse Number is: "<<Reverse_Number;
+}
