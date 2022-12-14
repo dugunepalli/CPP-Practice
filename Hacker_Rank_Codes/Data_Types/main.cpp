26ca48f3ab9c032aa113c986afeb815508e54bfd
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2022-12-14 12:10:59 +0530

    Create main.cpp

---
+++ Hacker_Rank_Codes/Data_Types/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int Number;
+    long Number1;
+    char Name;
+    float Number3;
+    double Number4;
+    cin>>Number>>Number1>>Name>>Number3>>Number4;
+    
+    cout<<setprecision(20)<<Number<<endl;
+    cout<<setprecision(20)<<Number1<<endl;
+    cout<<setprecision(20)<<Name<<endl;
+    cout<<setprecision(20)<<Number3<<endl;
+    cout<<setprecision(20)<<Number4;
+}
