51f279e22b4ad17429b0b79d88b3c70357ca5b10
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2022-12-14 12:22:54 +0530

    Create main.cpp

---
+++ Hacker_Rank_Codes/Conditional/main.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int Number;
+    cin>>Number;
+    
+    if(Number==1)
+    {
+        cout<<"one";
+    }
+    else if(Number==2)
+    {
+        cout<<"two";
+    }
+    else if(Number==3)
+    {
+        cout<<"three";
+    }
+    else if(Number==4)
+    {
+        cout<<"four";
+    }
+    else if(Number==5)
+    {
+        cout<<"five";
+    }
+    else if(Number==6)
+    {
+        cout<<"six";
+    }
+    else if(Number==7)
+    {
+        cout<<"seven";
+    }
+    else if(Number==8)
+    {
+        cout<<"eight";
+    }
+    else if(Number==9)
+    {
+        cout<<"nine";
+    }
+    else if(Number>9)
+    {
+        cout<<"Greater than 9";
+    }
+}
