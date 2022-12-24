452ef2eef8b918908f1dc02e80ca142947fccae4
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2022-12-24 18:29:24 +0530

    Create main.cpp

---
+++ Hacker_Rank_Codes/Greatest of 4 Numbers/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if((a>b)&&(a>c)&&(a>d))
+    {
+        cout<<a;
+    }
+    else if((b>a)&&(b>c)&&(b>d))
+    {
+        cout<<b;
+    }
+    else if((c>b)&&(c>a)&&(c>d))
+    {
+        cout<<c;
+    }
+    else {
+    {
+        cout<<d;
+    }
+    }
+}
