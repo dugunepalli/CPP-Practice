2759ea7f948f3a78e747d62145b92f823386cfd3
DUGUNEPALLI SUNIL KUMAR <116146270+dugunepalli@users.noreply.github.com>
2024-03-23 12:12:59 +0530

    Create 2.Access_Char_Array.cpp

---
+++ Data_Structures_&_Algorithms/1D Arrays/2.Access_Char_Array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char Sunil[5]={'a','b', 'c', 'd', 'e'};
+  for(int i=0;i<5;i++)
+  {
+    //printf("Sunil[%d] is on Index and Value is %d\n", i, Sunil[i]);
+    cout<<"Sunil["<<i<<"] is on Index and Value is "<<Sunil[i]<<endl;
+  }
+}
